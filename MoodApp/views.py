936ddbd4389ed7136e44e7db34454646e55db168
Casey Jones <155927499+Sk1ppyJones@users.noreply.github.com>
2026-01-24 18:12:51 -0700

    Basic Push
    First templates, and basic setup

--- MoodApp/views.py
+++ MoodApp/views.py
@@ -3,14 +3,4 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse(f"""
-                        <h1> 
-                            MoodFlow
-                        </h1>
-                        <p>MoodFlow is an app meant to allow
-                        the user to keep track of there current
-                        moods and act as a virtual journal. The
-                        hopeful goal of it is to help me keep track
-                        of manic episodes to talk to my therapist
-                        about :D</P
-                        """)
+    return render(request, 'MoodApp/index.html')
