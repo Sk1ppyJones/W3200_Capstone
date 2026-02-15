9bd36e24f62ce339cd2ad9545c26a69dffb70677
Casey Jones <155927499+Sk1ppyJones@users.noreply.github.com>
2026-02-15 12:50:58 -0700

    Commit

--- MoodApp/models.py
+++ MoodApp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=50)
+    streak = models.IntegerField(default=0)
+
+    def __str__(self):
+        return (f"User: {self.name}\nID: {self.userID}\nBirthdate: {self.birthdate} \nGender: {self.gender}\nMood Streak: {self.streak}")
+
+
+class Entry(models.Model):
+    entryDate = models.DateField()
+
+
+class MoodEntry(Entry):
+    # Thinking, each mood has a numeric 1-10 and then it assigns based off that
+    mood = models.IntegerField()
+    notes = models.CharField(max_length=120)
+
+    def __str__(self):
+        return (f'Entry ID: {self.ID}\nMood: {self.mood}\nNotes: {self.notes}')
+
+
+class JournalEntry(Entry):
+    title = models.CharField(max_length=50)
+    entryText = models.CharField(max_length=500)
+
+    def __str__(self):
+        return (f'Entry ID: {self.ID}\nTitle: {self.title}\n{self.entryText}')
