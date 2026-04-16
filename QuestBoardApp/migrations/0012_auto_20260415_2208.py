8c27121d55b622cfc6f2bdb34724494a41437bab
Casey Jones <155927499+Sk1ppyJones@users.noreply.github.com>
2026-04-15 22:16:29 -0600

    SuperUser try 2

--- QuestBoardApp/migrations/0012_auto_20260415_2208.py
+++ QuestBoardApp/migrations/0012_auto_20260415_2208.py
@@ -14,9 +14,6 @@ def create_superuser(apps, schema_editor):
     if username and email and password:
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            print(f"Superuser '{username}' created successfully.")
-        else:
-            print(f"Superuser '{username}' already exists.")
 
 
 class Migration(migrations.Migration):
